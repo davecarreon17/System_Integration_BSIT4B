710700915f79b97addb8e57a696653ac6ec5d12e
unknown <louisdavidcarreon91@gmail.com>
2026-08-31 21:45:22 +0800

    Update Project

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,43 @@
 <template>
-  <div>
-   <h1> Hello World</h1>
-   <button>submit</button>
-  </div>
+ <v-container>
+ <v-card v-if="user" class="pa-6">
+ <div class="d-flex align-center ga-4">
+ <v-avatar size="64">
+ <v-img :src="user.picture" />
+ </v-avatar>
+ <div>
+ <h2>{{ user.name }}</h2>
+ <p>{{ user.email }}</p>
+ </div>
+ </div>
+ </v-card>
+
+<v-btn color="error" @click="logout">
+ Logout
+</v-btn>
+
+
+ </v-container>
+
 </template>
 
 <script lang="ts" setup>
+
+const user = ref<any>(null)
+onMounted(() => {
+ const savedUser = localStorage.getItem('google_user')
+ if (savedUser) {
+ user.value = JSON.parse(savedUser)
+ }
+})
+
+const logout = () => {
+ localStorage.removeItem('google_user')
+ localStorage.removeItem('google_token')
+ navigateTo('/login')
+}
+
+
 
 </script>
 
